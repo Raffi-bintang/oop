90647dd0a987db0c718daa4491bfdabb6f6544dc
Raffi-bintang <raffibintang5@gmail.com>
2020-11-09 09:41:24 +0700

    uplod project Percabangan

---
+++ Percabangan/src/ifthenelse/Ifthenelse.java
@@ -0,0 +1,16 @@
+
+package ifthenelse;
+
+
+public class Ifthenelse {
+
+    public static void main(String[] args) {
+      boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Belum Bayar Listrik");
+        }
+    }
+    }
+ 
